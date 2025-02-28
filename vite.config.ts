a55b73fbb67cd97512542f586abb37b3a6462978
AnnaMelkova <anna.melkova@gmail.com>
2025-03-01 00:15:31 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Use the base path for production builds
+  base: mode === 'development' ? '/' : '/Anna-Melkova-CV/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
